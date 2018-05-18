59abf9fb166a39608143a89c80413714ee79e4f6
Daniel L.Branco <danlb48986@gmail.com>
2018-05-18 05:43:35 -0500

    Adding and using ViewModel

--- LibraryMgmtApp/Controllers/ProductsController.cs
+++ LibraryMgmtApp/Controllers/ProductsController.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LibraryMgmtApp.Models;
+using LibraryMgmtApp.ViewModels;
 
 namespace LibraryMgmtApp.Controllers
 {
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string sortBy)
         {
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
             var product = new Product()
             {
                 Name = "Carrie",
@@ -20,7 +24,19 @@ namespace LibraryMgmtApp.Controllers
                 ReleaseYear = 1976
             };
 
-            return View(product);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new IndexProductViewModel
+            {
+                Product = product,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ LibraryMgmtApp/ViewModels/IndexProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibraryMgmtApp.Models;
+
+namespace LibraryMgmtApp.ViewModels
+{
+    public class IndexProductViewModel
+    {
+        public Product Product { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
