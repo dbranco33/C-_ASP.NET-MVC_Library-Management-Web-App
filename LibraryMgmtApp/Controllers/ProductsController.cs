68aa61e325d07ed2545cb49587e70fbee8780a72
Daniel L.Branco <danlb48986@gmail.com>
2018-05-18 05:21:48 -0500

    Add 'ProductController' and its 'Index' view

---
+++ LibraryMgmtApp/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryMgmtApp.Models;
+
+namespace LibraryMgmtApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = new Product()
+            {
+                Name = "Carrie",
+                Author = "Stephen King",
+                Category = ProductCategory.Book,
+                ReleaseYear = 1976
+            };
+
+            return View(product);
+        }
+    }
+}
