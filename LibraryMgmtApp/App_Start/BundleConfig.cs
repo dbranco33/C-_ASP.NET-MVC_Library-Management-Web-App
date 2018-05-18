0a50a54b60d9cbed043a30c804e1529c10e1d900
Daniel L.Branco <danlb48986@gmail.com>
2018-05-18 05:33:12 -0500

    Adding new theme - lumen from bootswatch.com/3/

--- LibraryMgmtApp/App_Start/BundleConfig.cs
+++ LibraryMgmtApp/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace LibraryMgmtApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
