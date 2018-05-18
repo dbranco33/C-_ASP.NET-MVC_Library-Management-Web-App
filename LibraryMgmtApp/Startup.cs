05fdcb8ce0b98c4de76e58016a8909c3c12baf73
Daniel L.Branco <danlb48986@gmail.com>
2018-05-18 04:34:35 -0500

    Add project files.

---
+++ LibraryMgmtApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryMgmtApp.Startup))]
+namespace LibraryMgmtApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
