0fd7f5d8d823539bbb84f63a741f8bee8af4789e
Daniel L.Branco <danlb48986@gmail.com>
2018-05-22 15:47:55 -0500

    Creating an Edit form (and view), and an Edit action for the Customer controller.

--- LibraryMgmtApp/Controllers/CustomersController.cs
+++ LibraryMgmtApp/Controllers/CustomersController.cs
@@ -47,12 +47,12 @@ namespace LibraryMgmtApp.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -62,6 +62,27 @@ namespace LibraryMgmtApp.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            //checks if there's a customer with Id equals to parameter 'id'.
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
         }
     }
 }
--- LibraryMgmtApp/ViewModels/NewCustomerViewModel.cs
+++ LibraryMgmtApp/ViewModels/NewCustomerViewModel.cs
@@ -6,7 +6,7 @@ using LibraryMgmtApp.Models;
 
 namespace LibraryMgmtApp.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
